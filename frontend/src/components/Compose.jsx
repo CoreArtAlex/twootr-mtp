f0e2fb2a6781a10cfbb5b803403b171dc638089a
alejandroguerravillegas <coreartalex2@gmail.com>
2022-08-05 09:10:14 -0700

    modify

--- frontend/src/components/Compose.jsx
+++ frontend/src/components/Compose.jsx
@@ -3,31 +3,32 @@ import axios from 'axios';
 import {StyleCompose} from './styles/Compose.styles';
 
 export default function Compose(props){
-    // Construct the Twoot Text
     const { compose, setCompose } = props;
     const [composeChar, setComposeChar] = useState('');
 
     const handleSubmit = (event)=>{
         event.preventDefault();
         console.log('twooted', composeChar);
-        const newCompose = {
-            compose: composeChar,
+        const newTwoot = {
+            author:'',
+            content: composeChar,
+            authorSlug:'',
+            dateAdded:'',
         };
-        console.log('new compose',newCompose);
-        axios.get('/Compose').then((res)=>{
-            console.log('get twoots',res.data);
-            setCompose(res.data);
-        }).catch((error)=> console.log('error',error.response.data));
-        axios.post('/Compose',{ newCompose: compose })
+        console.log('new compose',newTwoot);
+
+        axios.post('/twoot',{ newTwoot })
         .then((res)=>{
             console.log('compose res',res);
             setCompose([...compose, {...res.data}]);
             console.log('twoots', setCompose);
         }).catch((error)=> console.log('error',error.response.data));
     };
+
     const handleInputChange = (e)=>{
         setComposeChar(e.target.value);
     };
+
     return (
         <StyleCompose>
         <div>
