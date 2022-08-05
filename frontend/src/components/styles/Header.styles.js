8ca96bd9d2b5d9fc19f295cc8dc5e30cce84fbda
Guilherme Maciel de Almeida <guilherme@gmaciel.com>
2022-08-05 14:07:00 -0700

    Add User and Header Initial

--- frontend/src/components/styles/Header.styles.js
+++ frontend/src/components/styles/Header.styles.js
@@ -1,8 +1,24 @@
 import styled from 'styled-components';
 
 export const StyleHeader = styled.header`
-    background-color: ${(props) => props.color};
-    label{
-        color: black;
+    background-color: #711a75;
+    color: #fff;
+    font-family: 'Kanit', sans-serif;
+    font-weight: 100;
+
+    div {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        padding: 1rem 2rem;
+        margin: 0;
+    }
+
+    .bold {
+        font-weight: 400;
+    }
+
+    .title {
+        font-family: 'Permanent Marker', cursive;
     }
 `;
--- frontend/src/components/styles/User.styles.js
+++ frontend/src/components/styles/User.styles.js
@@ -1,3 +1,46 @@
 import styled from 'styled-components';
 
-export const StyleUser = styled.div``;
+
+
+export const StyleUser = styled.div`
+   background-color: #413f42;
+   color: #fff;
+   font-family: 'Kanit', sans-serif;
+   font-weight: 100;
+   text-align: center;
+
+   img {
+      width: 10rem;
+   }
+
+   .name {
+      display: flex;
+      flex-direction: row;
+      padding: 0;
+      column-gap: 0.5rem;
+      justify-content: center;
+      align-items: center;
+   }
+
+   .bold {
+      font-weight: 400;
+   }
+
+   button {
+      background-color: #413f42;
+      border: none;
+      color: #fff;
+      cursor: pointer;
+      margin: 0;
+   }
+
+   .check {
+      background-color: green;
+      border-radius: 50%;
+      color: #413f42
+   }
+
+   .hide {
+      display: none;
+   }
+`;
